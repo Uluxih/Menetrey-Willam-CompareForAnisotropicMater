5ae6199abe198e7f30a598c315dd4bc28280de1f
Uluxih <ivsmag@vk.com>
2024-05-01 08:20:42 +0300

    накладывает точки

--- compare.py
+++ compare.py
@@ -10,34 +10,50 @@ from pyautocad import Autocad, APoint, ACAD
 
 
 def get_deviation(arr1, arr2):
-    dev=100
+    dev_sum=0
     for i in arr1:
+        dev=100
         for j in arr2:
-            dev_current = sqrt((arr1[0]**2-arr2[0])**2+(arr1[1]**2-arr2[1])**2)
+            dev_current = sqrt((i[0]**2-j[0])**2+(i[1]**2-j[1])**2)
+
             if dev_current<dev:
                 dev=dev_current
+        dev_sum=dev_sum+dev
     return dev
+
+def get_tuple(arrX, arrY):
+    i=1
+    tuple=[]
+    #print(arrX)
+    while i<len(arrX):
+        tuple.append([arrX[i], arrY[i]])
+        i=i+1
+    return  tuple
 # Задаю рисунок размером 7 на 7 и оси
 fig, ax = plt.subplots(figsize=(7, 7))
 a = 2
 ax.set_xlim(-16, 1)
 ax.set_ylim(-16, 1)
 # Точки с опытов Пейджа
-T = np.genfromtxt('Page_90.csv', delimiter=',')
+T = np.genfromtxt('Page_0.csv', delimiter=',')
 # Точки с кривой, которую записал точками
 WV = np.genfromtxt('points.csv', delimiter=',')
 # Точки с критерия прочности Гениева
-G = np.genfromtxt('points_G_90.csv', delimiter=',')
+G = np.genfromtxt('points_G_0.csv', delimiter=',')
 pointsX = T[:, 0] * -1  # Меняю х и у местами потому что у Пейджа СИГМА2 (по y) равно СИГМА1 у Поздеева
 pointsY = T[:, 1] * -1
 ax.scatter(pointsY, pointsX, marker="x", color="black", linewidths=1)
+pageArr=get_tuple(pointsX, pointsY)
+print(T)
 
 pointsX = WV[:, 0] * -1  # Меняю х и у местами потому что у Пейджа СИГМА2 (по y) равно СИГМА1 у Поздеева
 pointsY = WV[:, 1] * -1
 ax.scatter(pointsY, pointsX, marker=".", color="red", linewidths=0.01)
+mwArr=get_tuple(pointsX, pointsY)
 
 pointsX = G[:, 1] * 1
 pointsY = G[:, 0] * 1
+genArr=get_tuple(pointsX, pointsY)
 pointsX = pointsX.tolist()
 pointsY = pointsY.tolist()
 # Сгущаю точки с критерия Гениева, добавив одну точку между существующими
@@ -82,5 +98,8 @@ ax.minorticks_on()
 ax.grid(which='minor',
         color='gray',
         linestyle=':')
+
+print(get_deviation(pageArr, genArr))
+print(get_deviation(pageArr, mwArr))
 
 plt.show()
